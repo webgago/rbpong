1538c3d150536ab8ce4dc2d89a16afb370672fad
kiba <kiba@dell.linuxdev.us.dell.com>
2008-08-10 11:38:28 -0400

    initialize the control object

--- lib/setup.rb
+++ lib/setup.rb
@@ -20,6 +20,7 @@ class Setup
   def initialize
     @screen = Screen.new([800,600],0,[Rubygame::HWSURFACE,Rubygame::DOUBLEBUF])
     @queue = Rubygame::EventQueue.new()
+    @control = Controller.new(@screen)
   end
   def run
     loop do
