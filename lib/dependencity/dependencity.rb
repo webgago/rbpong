699007c8db751cdeb7ec0648c685d98334240056
kiba <wikipediankiba@gmail.com>
2009-09-02 21:00:36 -0700

    add process_rule method

--- lib/dependencity/dependencity.rb
+++ lib/dependencity/dependencity.rb
@@ -21,4 +21,18 @@ class Dependencity
     @files.pop_at(value)
     file.unshift(filename)
   end
+  def process_rules files
+    go_down = @rules.length()
+    go_down.times do |rule|
+      counter = 0
+      files.each do |file|
+        if file.match(@rules[go_down])
+          move_to_front(counter)
+        end
+        counter += 1
+      end
+      go_down -= 1
+    end
+    return files
+  end
 end
