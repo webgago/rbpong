ec6176164c570ed8772e24e169b78761a61ce270
kiba <wikipediankiba@gmail.com>
2009-09-06 16:09:26 -0700

    add loading code

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ require"yaml"
 require"lib/dependencity/dependencity.rb"
 include Rubygame
 TTF.setup()
+
+loader = Dependencity.new()
+loader.add_rule("top")
+loader.add_dir("lib/event.rb")
+loader.process_directories()
