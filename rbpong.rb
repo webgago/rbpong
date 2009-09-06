9c3a4368e70617d43fffafa942eac6ff05134e04
kiba <wikipediankiba@gmail.com>
2009-09-06 16:10:52 -0700

    replace with new code

--- rbpong.rb
+++ rbpong.rb
@@ -1,12 +1,1 @@
-#Rbpong is a public domain pong clone.
-
-require "rubygems"
-require "rubygame"
-require "lib/controller.rb"
-require "lib/setup.rb"
-include Rubygame
-#TTF.setup is necessary for using and manipulating texts and fonts.
-TTF.setup
-
-setup = Setup.new
-setup.run()
+require"config/environment.rb"
