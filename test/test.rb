c1450effc4ddee13c5561ce2262d2acf8b42e0a7
kiba <wikipediankiba@gmail.com>
2009-09-20 13:38:34 -0700

    add stuff

--- test/test.rb
+++ test/test.rb
@@ -2,9 +2,10 @@ require"config/environment.rb"
 loader = Dependencity.new()
 def all
   loader.add_dir("events")
+  loader.process_directories()
 end
 
 def load name
-  load.dir(name)
+  loader.dir(name)
+  loader.process_directories()
 end
-loader.process_directories()
