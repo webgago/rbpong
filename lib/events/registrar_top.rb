f54d777e2d47822c2b554f239c3d85d931f6f8e0
kiba <wikipediankiba@gmail.com>
2009-10-13 05:35:16 -0700

    add stuff

--- lib/events/registrar_top.rb
+++ lib/events/registrar_top.rb
@@ -3,4 +3,15 @@ class Registrar
   def initialize
     @hooks = []
   end
+  def construct_hook object , name, trigger =  Rubygame::EventTriggers::YesTrigger.new() , action = Rubygame::EventActions::MethodAction.new(:handle) , consumes = false, active = true
+    hook = {
+    :owner => object, 
+    :trigger => trigger,
+    :action => action,
+    :consumes => consumes,
+    :active => true,
+    :identifer => name
+    }
+    @hooks << hook
+  end
 end
